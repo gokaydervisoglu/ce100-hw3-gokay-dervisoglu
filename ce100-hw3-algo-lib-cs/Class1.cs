e483eda4fd4ada15a048fc11877953ffffa22c00
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-05-12 17:12:54 +0300

    Solution Created.

---
+++ ce100-hw3-algo-lib-cs/Class1.cs
@@ -0,0 +1,4 @@
+﻿namespace ce100_hw3_algo_lib_cs;
+public class Class1 {
+
+}
---
+++ ce100-hw3-algo-test-cs/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace ce100_hw3_algo_test_cs {
+public class UnitTest1 {
+  [Fact]
+  public void Test1() {
+  }
+}
+}
